f181366ad5a4c0bd0fa0bd00caade65791794311
ArchieBishopK <thelettaz@gmail.com>
2025-07-19 03:54:40 +0200

    Add iywd-theme/index.php 1752890078362

---
+++ iywd-theme/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main id="main-content" role="main">
+    <h1>Welcome to the Institute for Young Women Development</h1>
+    <p>This is the default template. Please assign a front page or create page templates.</p>
+</main>
+
+<?php get_footer(); ?>
